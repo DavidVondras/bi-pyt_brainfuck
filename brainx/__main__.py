8ea04a8cfac4f4f2f43cab737eee5c239d80eea8
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-12 10:23:51 +0200

    brainfuck

--- brainx/__main__.py
+++ brainx/__main__.py
@@ -1,4 +1,9 @@
 ﻿#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+import brainfuck as brfuck
+import brainloller as br
+if __name__ == "__main__":
+	#br.prp()
+	data = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+	brfuck.BrainFuck(data)
--- brainx/brainfuck.py
+++ brainx/brainfuck.py
@@ -1,18 +1,17 @@
-class BrainFuck
+class BrainFuck:
 	
 	def __init__(self, data):
 		self.data = data
 		mem = b'\x00'
 		self.memory = mem
-		self.memorzPtr = 0
+		self.memoryPtr = 0
 		self.output = ""
-		try:
-            with open(data, mode='r') as f:
-                self.bcode = f.read()
-        except:
-            self.bcode = data
+		self.bcode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
 		
 		self.interpreter(self.bcode)
 	
 	def interpreter( self, bcode ):
-		
+		print(bcode)
+		for i in range(0, len(bcode)):
+			#print(bcode[i])
+			self.calcMemory
