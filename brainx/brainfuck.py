422d18a151efef86bc35f59a091cf517f3ca4ff0
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-12 13:30:23 +0200

    brainfuck

--- brainx/brainfuck.py
+++ brainx/brainfuck.py
@@ -3,35 +3,33 @@ class BrainFuck:
 	def __init__(self, data):
 		self.data = data
 		mem = b'\x00'
-		self.memory = mem
+		self.memory = bytearray(mem)
 		self.memoryPtr = 0
 		self.output = ""
 		self.bcode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
 		
-		self.interpreter(self.bcode)
+		self.interpreter()
 	
 	def calcMemory( self, bChar ):
-		 if bChar == '+':
-            if self.memory[self.memoryPtr] != 255:
-				self.memory[self.memoryPtr] += 1
-            else:
+		print(self.memory[self.memoryPtr])
+		if bChar == '+':
+			if self.memory[self.memoryPtr] != 255:
+				print(self.memory[self.memoryPtr])	
+				self.memory[self.memoryPtr] += 1			
+			else:
 				self.memory[self.memoryPtr] = 0
-
-        elif bChar == '-':
-            if self.memory[self.Ptr] != 0:
-				self.memory[self.Ptr] -= 1
-            else:
-				self.memory[self.Ptr] = 255
+		elif bChar == '-':    
+			if self.memory[self.memoryPtr] != 0:
+				self.memory[self.memoryPtr] -= 1
+            
+			else:
+				self.memory[self.memoryPtr] = 255
+		
 		elif bChar == '>':
 			self.memoryPtr += 1
 			if len(self.memory) == self.memoryPtr:
 				self.memory += bytearray([0])		
 		
-		elif bChar == '>':
-			self.memory_pointer += 1
-            if len(self.memory) == self.memoryPtr:
-				self.memory += bytearray([0])
-
 		elif bChar == '<':
 			if self.memoryPtr > 0:
 				self.memoryPtr -= 1
@@ -39,11 +37,12 @@ class BrainFuck:
 		elif bChar == ',':
 			self.memory[self.memoryPtr] = ord(self._readchar())
 		
-		elif c[c_i] == '.':
-			print(chr(self.memory[self.memoryPtr]), end=r'')
+		elif bChar == '.':
+			print("+" + chr(self.memory[self.memoryPtr]), end=r'')
 			self.output += chr(self.memory[self.memoryPtr])
 		
-	def interpreter( self, bcode ):
-		print(bcode)
-		for i in range(0, len(bcode)):
-			self.calcMemory(bcode[i])
+		
+	def interpreter( self ):
+		print(self.bcode)
+		for i in range(0, len(self.bcode)):
+			self.calcMemory(self.bcode[i])
