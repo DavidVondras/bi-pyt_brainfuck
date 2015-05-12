0c5b7647e87a005045efd7d7d934a6fef63f4a0c
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-12 15:23:35 +0200

    brainfuck

--- brainx/brainfuck.py
+++ brainx/brainfuck.py
@@ -1,48 +1,57 @@
-class BrainFuck:
-	
-	def __init__(self, data):
-		self.data = data
-		mem = b'\x00'
-		self.memory = bytearray(mem)
-		self.memoryPtr = 0
-		self.output = ""
-		self.bcode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
-		
-		self.interpreter()
-	
-	def calcMemory( self, bChar ):
-		print(self.memory[self.memoryPtr])
-		if bChar == '+':
-			if self.memory[self.memoryPtr] != 255:
-				print(self.memory[self.memoryPtr])	
-				self.memory[self.memoryPtr] += 1			
-			else:
-				self.memory[self.memoryPtr] = 0
-		elif bChar == '-':    
-			if self.memory[self.memoryPtr] != 0:
-				self.memory[self.memoryPtr] -= 1
-            
-			else:
-				self.memory[self.memoryPtr] = 255
-		
-		elif bChar == '>':
-			self.memoryPtr += 1
-			if len(self.memory) == self.memoryPtr:
-				self.memory += bytearray([0])		
-		
-		elif bChar == '<':
-			if self.memoryPtr > 0:
-				self.memoryPtr -= 1
+import sys
 
-		elif bChar == ',':
-			self.memory[self.memoryPtr] = ord(self._readchar())
-		
-		elif bChar == '.':
-			print("+" + chr(self.memory[self.memoryPtr]), end=r'')
-			self.output += chr(self.memory[self.memoryPtr])
-		
-		
-	def interpreter( self ):
-		print(self.bcode)
-		for i in range(0, len(self.bcode)):
-			self.calcMemory(self.bcode[i])
+
+class BrainFuck:
+
+    def __init__(self, data):
+        self.data = data
+        mem = b'\x00'
+        self.memory = bytearray(mem)
+        self.memoryPtr = 0
+        self.output = ""
+        self.bcode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+        self.interpreter(self.bcode)
+
+    def interpreter(self, bcode):
+        i = 0
+        while i < len(bcode):
+            if bcode[i] == '>':
+                self.memoryPtr += 1
+                if len(self.memory) == self.memoryPtr:
+                    self.memory += bytearray([0])
+            if bcode[i] == '<':
+                if self.memoryPtr > 0:
+                    self.memoryPtr -= 1
+            if bcode[i] == '+':
+                self.memory[self.memoryPtr] += 1
+                if self.memory[self.memoryPtr] == 256:
+                    self.memory[self.memoryPtr] = 0
+            if bcode[i] == '-':
+                self.memory[self.memoryPtr] -= 1
+                if self.memory[self.memoryPtr] == -1:
+                    self.memory[self.memoryPtr] = 255
+            if bcode[i] == '.':
+                print(chr(self.memory[self.memoryPtr]))
+                self.output += chr(self.memory[self.memoryPtr])
+            if bcode[i] == ',':
+                self.memory[self.memoryPtr] = ord(self.getchar())
+            if bcode[i] == '[':
+                code = self.cycle(bcode[i:])
+                while self.memory[self.memoryPtr] != 0:
+                    self.interpreter(code)
+                i += len(code) + 1
+            i += 1
+
+    def cycle(self, bcode):
+        end = 1
+        while (bcode[0:end].count('[') != bcode[0:end].count(']')):
+            end += 1
+        return bcode[1:end - 1]
+
+    def getchar(self):
+        if len(self.user_input) == 0:
+            return sys.stdin.read(1)
+        else:
+            ret = self.user_input[0]
+            self.user_input = self.user_input[1:]
+            return ret
