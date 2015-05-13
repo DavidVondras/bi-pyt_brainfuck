72723996fd4fc8d54a47912570a72b0ff5921e6e
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-13 12:42:42 +0200

    brainloller

--- brainx/brainloller.py
+++ brainx/brainloller.py
@@ -15,11 +15,14 @@ class BrainLoller():
         start = 0, 0
         m = 0, 1
         ret = ''
-        while not self._out(self.rgb, start):
+        while not self.out(start):
             o, m = self.getChars(self.rgb[start[0]][start[1]], m)
             ret += o
             start = start[0] + m[0], start[1] + m[1]
         return ret
+
+    def out(self, p):
+        return p[0] == len(self.rgb) or p[1] == len(self.rgb[0]) or p[0] < 0 or p[1] < 0
 
     def getChars(self, color, m):
         ch = ''
