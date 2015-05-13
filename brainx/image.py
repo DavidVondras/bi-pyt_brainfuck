1618b5fbab61a1283f3652fde8128d67d846508d
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-13 12:35:07 +0200

    image

--- brainx/image.py
+++ brainx/image.py
@@ -12,6 +12,30 @@ class PNGNotImplementedError(Exception):
 
 
 class ImagePng():
+    def getBytes(self, bytes):
+        n = 0
+        for b in bytes:
+            n = n * 256 + b
+        return n
+
+    def pixelTG(self, a, left, up, ul):
+        res = tuple()
+        for i in range(0, 3):
+            p = (left[i] + up[i] - ul[i])
+            pl = abs(p - left[i])
+            pu = abs(p - up[i])
+            pul = abs(p - ul[i])
+
+            if (pl <= pu and pl <= pul):
+                res += (left[i],)
+            elif (pu <= pul):
+                res += (up[i],)
+            else:
+                res += (ul[i],)
+        x = (a[0] + res[0]) % 256
+        z = (a[1] + res[1]) % 256
+        y = (a[2] + res[2]) % 256
+        return (x, z, y)
 
     def __init__(self, filename):
         fileImage = open(filename, mode='rb')
@@ -46,27 +70,27 @@ class ImagePng():
         idat = zlib.decompress(idat)
         self.rgb = []
         p = 0
-        for row in range(0,self.height):
+        for row in range(0, self.height):
             pngF = idat[p]
             p += 1
             line = []
-            left = (0,0,0)
-            upleft = (0,0,0)
-            for c in range(0,self.width):
-                pi = (idat[p],idat[p+1],idat[p+2])
+            left = (0, 0, 0)
+            upleft = (0, 0, 0)
+            for c in range(0, self.width):
+                pi = (idat[p], idat[p + 1], idat[p + 2])
                 p += 3
                 if (pngF == 0):
                     left = pi
                     line += [pi]
                 elif (pngF == 1):
-                    left = self.pixelTG(left,pi)
+                    left = self.pixelTG(left, pi)
                     line += [left]
                 elif (pngF == 2):
-                    left = self.pixelTG(pi,self.rgb[len(self.rgb)-1][c])
+                    left = self.pixelTG(pi, self.rgb[len(self.rgb) - 1][c])
                     line += [left]
 
                 elif (pngF == 4):
-                    up = self.rgb[len(self.rgb)-1][c]
+                    up = self.rgb[len(self.rgb) - 1][c]
                     current = self.pixelTG(pi, left, up, upleft)
                     line += [current]
                     left = current
