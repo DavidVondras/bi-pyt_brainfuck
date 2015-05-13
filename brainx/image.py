006d0cddda8c43ceb46e779b12b97f090655ec63
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-13 10:55:45 +0200

    image

--- brainx/image.py
+++ brainx/image.py
@@ -13,4 +13,11 @@ class ImagePng():
         self.imageValues = fileImage.read()
         if (self.binary[:8] != b'\x89PNG\r\n\x1a\n'):
             raise PNGWrongHeaderError()
-        
+        self.imageValues = self.imageValues[8:]
+        self.data = []
+        p = 0
+        while p < len(self.imageValues):
+            l = self.getBytes(self.imageValues[p:p + 4])
+            p += 4
+            self.data += [{'head':self.imageValues[p:p + 4], 'data':self.imageValues[p + 4:p + l + 4]}]
+            p += (l + 8)
