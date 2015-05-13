7fac68c66baefb7f3c0aa808eea3610cec643137
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-13 10:51:33 +0200

    image

--- brainx/image.py
+++ brainx/image.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
+class PNGWrongHeaderError(Exception):
+    pass
+
+
 class ImagePng():
 
     def __init__(self, filename):
         fileImage = open(filename, mode='rb')
         self.imageValues = fileImage.read()
-        
+        if (self.binary[:8] != b'\x89PNG\r\n\x1a\n'):
+            raise PNGWrongHeaderError()
         
