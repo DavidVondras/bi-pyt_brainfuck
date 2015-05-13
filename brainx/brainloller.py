686f04a2a14aeac016f034aae66c45dcb1fe95c9
DavidVondras <vondrda9@fit.cvut.cz>
2015-05-13 12:40:01 +0200

    brainloller

--- brainx/brainloller.py
+++ brainx/brainloller.py
@@ -5,12 +5,22 @@ import image
 
 class BrainLoller():
     def __init__(self, filename):
-        self.data = getBrainLoller(filename)
-        
+        self.data = self.getBrainLoller(filename)
+
     def readImage(self, filename):
         self.rgb = image.ImagePng(filename).rgb
 
     def getBrainLoller(self, filename):
+        self.readImage(filename)
+        start = 0, 0
+        m = 0, 1
+        ret = ''
+        while not self._out(self.rgb, start):
+            o, m = self.getChars(self.rgb[start[0]][start[1]], m)
+            ret += o
+            start = start[0] + m[0], start[1] + m[1]
+        return ret
+
     def getChars(self, color, m):
         ch = ''
         if color == (255, 0, 0):
@@ -34,7 +44,6 @@ class BrainLoller():
         if color == (0, 128, 128):
             m = self._turn(m, "left")
         return ch, m
-    def 
 
 if __name__ == "__main__":
-    BrainLoller.readImage("../tests/HelloWord.png")
+    BrainLoller("../tests/HelloWord.png")
